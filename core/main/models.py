da9c1039d6997e78652f717c6fc8ad0082fe10bb
Fahd Mohammed <fahdmoh.1@gmail.com>
2023-08-21 22:34:47 +0100

    added the views and respectives urls

--- core/main/models.py
+++ core/main/models.py
@@ -11,3 +11,38 @@ class Staff(models.Model):
 	def __str__(self):
 		return self.name
 	
+
+
+
+class Service(models.Model):
+	name = models.CharField(max_length=255)
+	description = models.TextField()
+	image = models.ImageField(upload_to="services/")
+
+	def __str__(self):
+		return self.name
+	
+
+class Contact(models.Model):
+	name= models.CharField(max_length=255)
+	email = models.EmailField()
+	phone = models.CharField(max_length=15)
+	message = models.TextField()
+
+	def __str__(self):
+		return self.name
+
+
+
+class Appointment(models.Model):
+	customer_name = models.CharField(max_length=255)
+	customer_meail = models.EmailField()
+	customer_phone = models.CharField(max_length=15)
+	service_type = models.CharField(max_length=255)
+	date = models.DateField()
+	time = models.TimeField()
+	service = models.ForeignKey(Service, on_delete=models.CASCADE, null=True, blank= True)
+	staff = models.ForeignKey(Staff, on_delete=models.CASCADE, null= True, blank=True)
+
+	def __str__(self) -> str:
+		return self.customer_name
--- core/main/urls.py
+++ core/main/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 app_name = "main"
 urlpatterns =[
-    
+    path('', BaseView.as_view(), name="base"),
+    path('contact-us/',ContactUsView.as_view(), name="contact_us"),
+    path('book-appointment/',AppointmentView.as_view(), name="appointment"), #noqa
 ]
