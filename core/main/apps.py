430a21c69e17ceddc663fcab2cc7147d6c3bfb60
Fahd Mohammed <fahdmoh.1@gmail.com>
2023-08-22 21:25:06 +0100

    added signals to send SMS

--- core/main/apps.py
+++ core/main/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class MainConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "main"
+	default_auto_field = "django.db.models.BigAutoField"
+	name = "main"
+
+	#load signals when app is initialized
+	def ready(self):
+		from . import signals
+		
---
+++ core/main/signals.py
@@ -0,0 +1,48 @@
+import array 
+
+import requests
+#from core import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import core.settings as settings
+from .models import Contact,Appointment,Staff
+
+@receiver(post_save, sender=Contact)
+def notify_about_contac(sender, instance, created, **kwargs):
+	staffs = Staff.objects.all()
+	# make a list of all phone numbers of staff numbers
+	recipients = [staff.phone for staff in staffs]
+	if created:
+		send_sms(settings.SMS_SENDER_ID,
+				 f"New contact from {instance.name} with email {instance.email}\n{instance.message} ", recipients)
+		
+
+
+@receiver(post_save, sender=Appointment)
+def notify_about_appointment(sender, instance, created, **kwargs):
+	staffs = Staff.objects.all()
+
+	#make a list of all phone numbers of staff members
+	staff_recipients = [staff.phone for staff in staffs]
+	customer_receipient = [str(instance.customer_phone)]
+	staff_message = f"New appointment from {instance.customer_name} with email {instance.customer_email} for {instance.service}({instance.service_type}) on {instance.date} at {instance.time}"
+	customer_message = f"Dear {instance.customer_name}, your appointment for{instance.service}({instance.service_type}) on {instance.date} at {instance.time} has been received. We will get back to you shortyl"
+
+	if created:
+		#send to staff
+		send_sms(settings.SMS_SENDER_ID, staff_message, staff_recipients)
+		#send to customer
+		send_sms(settings.SMS_SENDER_ID, customer_message, customer_receipient)
+
+def send_sms(sender:str, message:str, receipients: array.array):
+	header = {"api-key": settings.ARKESEL_API_KEY, 'Content-Type': 'application/json',
+              'Accept': 'application/json'}
+	
+	SEND_SMS_URL = "https;//sms.arkesel.com/api/v2/sms/sned"
+	payload = {
+		"sender": sender,
+		"message": message,
+		"recipients": receipients
+	}
+	response = requests.post(SEND_SMS_URL,headers=header, json=payload)
+	return response.json()
